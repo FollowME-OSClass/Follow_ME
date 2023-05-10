ec73edab75ebca7ab1ac37fa3c1adeb0e92660ac
InZury <InZury0923@gmail.com>
2023-05-10 22:51:48 +0900

    카카오 로그인 구현 중

--- Follow_Me/app/src/main/java/com/example/follow_me/MainActivity.kt
+++ Follow_Me/app/src/main/java/com/example/follow_me/MainActivity.kt
@@ -1,12 +1,53 @@
-package com.example.follow_me
+package com.FollowMe.project
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.kakao.sdk.auth.model.OAuthToken
+import com.kakao.sdk.common.KakaoSdk
+import com.kakao.sdk.common.util.Utility
+import com.kakao.sdk.user.UserApiClient
+import com.kakao.sdk.common.model.ClientError
+import com.kakao.sdk.common.model.ClientErrorCause
 
-class MainActivity : AppCompatActivity() {
+const val TAG = "Kakao Login"
+
+class MainActivity : AppCompatActivity()
+{
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val callback: (OAuthToken?, Throwable?) -> Unit =
+            {
+                token, error ->
+                if(error != null) Log.e(TAG, "카카오 계정으로 로그인 실패", error)
+                else if(token != null) Log.i(TAG, "카카오 계정으로 로그인 성공 ${token.accessToken}")
+            }
+
+        // 카카오톡이 설치되어 있으면 카카오톡으로 로그인, 아니면 카카오계정으로 로그인
+        if(UserApiClient.instance.isKakaoTalkLoginAvailable(this))
+        {
+            UserApiClient.instance.loginWithKakaoTalk(this)
+            {
+                token, error ->
+                if (error != null)
+                {
+                    Log.e(TAG, "카카오톡으로 로그인 실패", error)
+
+                    // 사용자가 카카오톡 설치 후 디바이스 권한 요청 화면에서 로그인을 취소한 경우,
+                    // 의도적인 로그인 취소로 보고 카카오계정으로 로그인 시도 없이 로그인 취소로 처리 (예: 뒤로 가기)
+                    if (error is ClientError && error.reason == ClientErrorCause.Cancelled) return@loginWithKakaoTalk
+
+                    // 카카오톡에 연결된 카카오계정이 없는 경우, 카카오계정으로 로그인 시도
+                    UserApiClient.instance.loginWithKakaoAccount(this, callback = callback)
+                }
+                else if (token != null) Log.i(TAG, "카카오톡으로 로그인 성공 ${token.accessToken}")
+            }
+        }
     }
+
+
+
 }
