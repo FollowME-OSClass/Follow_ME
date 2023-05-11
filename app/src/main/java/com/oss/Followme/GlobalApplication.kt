eca96c13d649248075341dd2b85b3a54dc7e867f
InZury <InZury0923@gmail.com>
2023-05-11 19:49:06 +0900

    카카오 로그인 구현

---
+++ app/src/main/java/com/oss/Followme/GlobalApplication.kt
@@ -0,0 +1,12 @@
+package com.oss.Followme
+
+import android.app.Application
+import com.kakao.sdk.common.KakaoSdk
+
+class GlobalApplication :Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        KakaoSdk.init(this, "652680317a3f2a98bb6ab7132a8bd4c2")
+    }
+}
--- app/src/main/java/com/oss/Followme/MainActivity.kt
+++ app/src/main/java/com/oss/Followme/MainActivity.kt
@@ -1,11 +1,66 @@
 package com.oss.Followme
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.ImageButton
+import androidx.appcompat.app.AppCompatActivity
+import com.kakao.sdk.auth.model.OAuthToken
+import com.kakao.sdk.common.model.ClientError
+import com.kakao.sdk.common.model.ClientErrorCause
+import com.kakao.sdk.user.UserApiClient
+import com.kakao.sdk.common.util.Utility
 
-class MainActivity : AppCompatActivity() {
+const val KTAG = "KakaoLog"
+public class MainActivity : AppCompatActivity(), View.OnClickListener
+{
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val kakaoLogin = findViewById<ImageButton>(R.id.KakaoLogin)
+        kakaoLogin.setOnClickListener(this)
+
+
+        val keyHash = Utility.getKeyHash(this)
+
+        Log.i("KeyHash", keyHash)
+    }
+
+    override fun onClick(v: View?) {
+
+        if(v != null)
+        {
+            when(v.id)
+            {
+                R.id.KakaoLogin->
+                {
+                    Log.i(KTAG, "버튼 테스트")
+                    val callback: (OAuthToken?, Throwable?) -> Unit =
+                        {
+                            token, error ->
+                            if(error != null) Log.e(KTAG, "카카오 계정으로 로그인 실패", error)
+                            else if(token != null) Log.i(KTAG, "카카오 계정으로 로그인 성공")
+                        }
+
+                    if(UserApiClient.instance.isKakaoTalkLoginAvailable(this))
+                    {
+                        UserApiClient.instance.loginWithKakaoTalk(this)
+                        {
+                            token, error ->
+                            if(error != null)
+                            {
+                                Log.e(KTAG, "카카오톡으로 로그인 실패", error)
+
+                                if(error is ClientError && error.reason == ClientErrorCause.Cancelled) return@loginWithKakaoTalk
+                            }
+                            else if(token != null) Log.i(KTAG, "카카오톡으로 로그인 성공 ${token.accessToken}")
+                        }
+                    }
+                    else UserApiClient.instance.loginWithKakaoAccount(this, callback = callback)
+                }
+            }
+        }
+
     }
 }
